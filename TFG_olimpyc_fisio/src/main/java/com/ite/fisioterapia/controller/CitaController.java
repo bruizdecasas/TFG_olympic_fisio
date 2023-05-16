28453231eb1cc747f3502ca88cddb749978cba2e
b.ruizdecasas <98181034+bruizdecasas@users.noreply.github.com>
2023-05-16 08:44:44 +0200

    Update CitaController.java

--- TFG_olimpyc_fisio/src/main/java/com/ite/fisioterapia/controller/CitaController.java
+++ TFG_olimpyc_fisio/src/main/java/com/ite/fisioterapia/controller/CitaController.java
@@ -17,176 +17,228 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 import com.ite.fisioterapia.entities.Cita;
-import com.ite.fisioterapia.entities.Reserva;
 import com.ite.fisioterapia.entities.Usuario;
 import com.ite.fisioterapia.service.CitaService;
-import com.ite.fisioterapia.service.ReservaService;
 import com.ite.fisioterapia.service.UsuarioService;
 
 /**
- * This is the controller relative to all operations performed around Reservas
+ * This is the controller relative to all operations performed around Citas
  */
 @Controller
-@RequestMapping("/reserva")
-public class ReservaController {
+@RequestMapping("/cita")
+public class CitaController {
 	
-	@Autowired ReservaService reserserv;
-	@Autowired private UsuarioService usuarioServ;
 	@Autowired private CitaService citaserv;
+	@Autowired private UsuarioService usuarioServ;
 	
 	/**
-	 * Returns a list of all the Reservas in the database.
+	 * Returns a list of all the Citas in the database.
 	 * @author: Benjamín Ruiz
-	 * @param model the Model object to be populated with the list of Reservas
-	 * @return the name of the view for displaying the list of Reservas
+	 * @param model the Model object to be populated with the list of Citas
+	 * @return the name of the view for displaying the list of Citas
 	 */
 	@GetMapping("/todas")
-	public String mostrarReservas (Model model) {
-		List<Reserva> reserva = reserserv.findAll();
-		model.addAttribute("listareservas", reserva);
-		return "/reservas/listaReservas";
+	public String mostrarCitas (Model model) {
+		List<Cita> cita = citaserv.findAll();
+		model.addAttribute("listacitas", cita);
+		List<Usuario> usuario = usuarioServ.findByRol(2);
+		model.addAttribute("listaUsuarios", usuario);
+		return "/reservas/listaCitas";
 	}
 	
 	/**
-	 * Returns a list of all the Reservas in the database relative to the user who is connected.
+	 * Returns a list of all the Citas in the database relative to the user who created them.
 	 * @author: Benjamín Ruiz
-	 * @param aut the Authentication represents the authenticated user
-	 * @param model the Model object to be populated with the list of Reservas
-	 * @return the name of the view for displaying the list of Reservas
+	 * @param model the Model object to be populated with the list of Citas
+	 * @return the name of the view for displaying the list of Citas
 	 */
-	@GetMapping("/misResevas")
-	public String mostrarMisReservas (Model model, Authentication aut, HttpSession misesion) {
-		Usuario usuario = usuarioServ.findByEmail(aut.getName());
-		List<Reserva> reserva = reserserv.findByUsuario(usuario.getIdUsuario());
-		model.addAttribute("listareservas", reserva);
-		return "/reservas/listaReservas";
+	@GetMapping("/todas/{id}")
+	public String mostrarCitasEspecialista (Model model,@PathVariable(name = "id") int codigo) {
+		List<Cita> cita = citaserv.findByEspecialista(codigo);
+		model.addAttribute("listacitas", cita);
+		return "/reservas/listaCitas";
+	}
+	
+	
+	
+	/**
+	 * Returns a list of all available Citas in the database.
+	 * @author: Benjamín Ruiz
+	 * @param model the Model object to be populated with the list of available Citas 
+	 * @return the name of the view for displaying the list of available Citas 
+	 */
+	@GetMapping ("/disponible")
+	public String mostrarCitasDisponibles (Model model) {
+		List<Cita> cita = citaserv.findByDisponible(1);
+		model.addAttribute("listacitas", cita);
+		return "/reservas/listaCitasDisponibles";
 	}
 	
 	/**
-	 * Retrieves the details of a specific Reserva using a GET request.
+	 * Retrieves the details of a specific Cita using a GET request.
 	 * @author: Benjamín Ruiz
-	 * @param model the Model object to which the Reserva attribute will be added.
-	 * @param codigo the ID of the Reserva to retrieve.
-	 * @return the name of the view that displays the Reserva details.
+	 * @param model the Model object to which the Cita attribute will be added.
+	 * @param codigo the ID of the Cita to retrieve.
+	 * @return the name of the view that displays the Cita details.
 	 */	
 	@GetMapping("/detalle/{id}")
-	public String detalleReserva(Model model, @PathVariable(name="id") int codigo) {
-		Reserva reserva = reserserv.findById(codigo);
-		model.addAttribute("reserva", reserva);
-		return "/reservas/detalleReserva";
+	public String detalleCita(Model model, @PathVariable(name="id") int codigo) {
+		Cita cita = citaserv.findById(codigo);
+		model.addAttribute("cita", cita);
+		return "/reservas/detalleCita";
 	}	
 	
 	/**
-	 * Retrieves the Reserva data to be edited in the form.
+	 * Retrieves the Cita data to be edited in the form.
 	 * @author: Benjamín Ruiz
-	 * @param model the model to be used to add the Reserva to be displayed in the form
-	 * @param codigo the ID of the Reserva to be edited
-	 * @return the path of the view to display the form for editing the Reserva data
+	 * @param model the model to be used to add the Cita to be displayed in the form
+	 * @param codigo the ID of the Cita to be edited
+	 * @return the path of the view to display the form for editing the Cita data
 	 */
 	@GetMapping("/editar/{id}")
-	public String editarReserva (Model model, @PathVariable(name="id") int  codigo) {
-		Reserva reserva = reserserv.findById(codigo);
-		model.addAttribute("reserva", reserva);
-		return "/reservas/editarReserva";
+	public String editarCita (Model model, @PathVariable(name="id") int  codigo) {
+		Cita cita = citaserv.findById(codigo);
+		model.addAttribute("cita", cita);
+		return "/reservas/editarCita";
 	}
 
+
 	/**
-	 * Handles the POST request to edit an existing Reserva.
+	 * Handles the POST request to edit an existing Cita.
 	 * @author: Benjamín Ruiz
-	 * @param reserva The Reserva to be edited.
+	 * @param aut: an Authentication object that represents the authentication information of the user making the request.
+	 * @param cita The Cita to be edited.
+	 * @param model Object that holds the attributes to be used in the view.
 	 * @param attr Object that contains attributes for the redirect page.
 	 * @return A String representing the redirect page URL.
 	 */
 	@PostMapping("/editar")
-	public String editarReserva (Reserva reserva, RedirectAttributes attr) {
-	    int resultado = reserserv.editarReserva(reserva);
-	    if (resultado == 1) {
-	        attr.addFlashAttribute("mensajeExito", "Reserva editada correctamente");
-	    } else if (resultado == 2) {
-	        attr.addFlashAttribute("mensajeError", "Reserva no encontrada");
-	    } else {
-	        attr.addFlashAttribute("mensajeError", "Error al editar la reserva");
-	    }
-		return "/";
+	public String editarCita (Authentication aut, Cita cita, RedirectAttributes attr, Model model) {
+		cita.setDisponible(1);
+		Usuario usuario = usuarioServ.findByEmail(aut.getName());
+		cita.setUsuario(usuario);
+		if (citaserv.editarCita(cita) == 1) 
+			attr.addFlashAttribute("mensajeExito", "Cita editada correctamente");
+		else if (citaserv.editarCita(cita) == 2) 
+			attr.addFlashAttribute("mensajeError", "Cita no encontrada");
+		else
+			attr.addFlashAttribute("mensajeError", "error al editar la cita");
+		return "redirect:/";
 	}
 	
 	/**
-	 * Displays the registration form for a new Reserva 
+	 * Displays the registration form for a new Cita, It retrieves the logged in user's information and adds it to the model.
 	 * @author: Benjamín Ruiz
-	 * @param model the Model object to which the Reserva attribute will be added.
-	 * @return the name of the view for registering a new Reserva .
+	 * @param aut the authentication object containing the Usuario´s credentials
+	 * @param model the model object to be used in the view
+	 * @param misesion the HttpSession object for the current user session
+	 * @return the name of the view to display the Cita creation form
 	 */	
-	@GetMapping ("/alta/{id}")
-	public String altaReserva(Model model, @PathVariable(name="id") int  codigo) {
-		Cita cita = citaserv.findById(codigo);
-		model.addAttribute("cita", cita);
-		return "/reservas/altaReserva";
+	@GetMapping ("/alta")
+	public String altaCita(Authentication aut, Model model, HttpSession misesion) {
+		Usuario usuario = usuarioServ.findByEmail(aut.getName());
+		model.addAttribute("usuario", usuario);
+		return "/reservas/altaCita";
 	}
-	
+		
 	/**
-	 * This method handles the HTTP POST request for creating a new Reserva.It receives a Reserva object from the request's body, 
-	 * tries to add it to the database via the reserserv service,and returns a redirect to the homepage with a success or error message.
+	 * This method handles the HTTP POST request for creating a new Cita.It receives a Cita object from the request's body, 
+	 * tries to add it to the database via the citaserv service,and returns a redirect to the homepage with a success or error message.
 	 * @author: Benjamín Ruiz
-	 * @param reserva the Reserva object representing the new Reserva to be added
-	 * @param idCita The ID of the associated Cita.
+	 * @param aut: an Authentication object that represents the authentication information of the user making the request.
+	 * @param cita the Cita object representing the new Cita to be added
 	 * @param attr a RedirectAttributes object used to send flash attributes to the redirected page
 	 * @param model the Model object used to add attributes to the view
 	 * @return a String representing the URL to be redirected to after the POST request is processed
 	 */	
 	@PostMapping("/alta")
-	public String altaReserva (@RequestParam("idCita") int idCita, Reserva reserva, RedirectAttributes attr, Model model, Authentication aut, HttpSession misesion) {
-	    Usuario usuario = usuarioServ.findByEmail(aut.getName());
-	    reserva.setUsuario(usuario);
-	    Cita cita = citaserv.findById(idCita);
-	    reserva.setCita(cita);	
-	    if (reserserv.altaReserva(reserva) == 1) {
-	        model.addAttribute("mensajeExito", "Reserva dada de alta correctamente");
-	        Cita citaReservada = reserva.getCita();
-	        citaReservada.setDisponible(0);
-	        citaserv.editarCita(citaReservada);
-	    } else { 
-	        model.addAttribute("mensajeError", "Error al dar de alta la reserva");
-	    }   
-	    return "redirect:/";
-	}
-	
-	/**
-	 * Handles a GET requests to cancell a Reserva with the given ID. 
-	 * This method deletes the Reserva and makes the associated Cita available again.
-	 * @author: Benjamín Ruiz
-	 * @param codigo the ID of the Reserva to be cancelled.
-	 * @param attr Object that contains attributes for the redirect page.
-	 * @return A String representing the redirect page URL.
-	 */
-	@GetMapping("/cancelar/{id}")
-	public String cancelarReserva (@PathVariable(name="id") int codigo, RedirectAttributes attr) {
-		Reserva reserva = reserserv.findById(codigo);
-		Cita cita = reserva.getCita();
+	public String altaCita (Authentication aut, Cita cita, RedirectAttributes attr, Model model, HttpSession session) {
 		cita.setDisponible(1);
-		reserserv.eliminarReserva(codigo);
-		attr.addFlashAttribute("mensajeExito", "Has cancelado la reserva");
+		Usuario usuario = usuarioServ.findByEmail(aut.getName());
+		cita.setUsuario(usuario);
+		if (citaserv.altaCita(cita) == 1) 
+			attr.addFlashAttribute("mensajeExito", "Cita dada de alta correctamente");
+		else 
+			attr.addFlashAttribute("mensajeError", "Error al dar de alta la cita");
 		return "redirect:/";
 	}
 	
+
 	/**
-	 * Handles a GET requests to delete a Reserva with the given ID.
+	 * Handles a GET request to reserve a specific Cita identified by the given "id" parameter.
+	 * If the Cita does not exist or has already been reserved, a message is added to the redirect
+	 * attributes and the user is redirected to the main page. Otherwise, the Cita is marked as reserved
+	 * and a success message is added to the redirect attributes. Finally, the user is redirected to the main page.
 	 * @author: Benjamín Ruiz
-	 * @param codigo the ID of the Reserva to be deleted.
+	 * @param cita the Cita object representing the new Cita to be reserved
+	 * @param attr a RedirectAttributes object used to send flash attributes to the redirected page
+	 * @param model the Model object used to add attributes to the view
+	 * @return a String representing the URL to be redirected
+	 */	
+	@GetMapping("/reserva/{id}")
+	public String reservarCita(@PathVariable(name="id") int codigo, RedirectAttributes attr) {
+	    Cita cita = citaserv.findById(codigo);
+	    if (cita == null) {
+	        attr.addFlashAttribute("mensajeError", "La cita no existe");
+	        return "redirect:/";
+	    } else if (cita.getDisponible() == 0) {
+	        attr.addFlashAttribute("mensajeError", "La cita ya ha sido reservada");
+	        return "redirect:/";
+	    } else {
+	        cita.setDisponible(0);
+	        citaserv.editarCita(cita);
+	        attr.addFlashAttribute("mensajeExito", "Cita reservada con éxito");
+	        return "redirect:/";
+	    }
+	}
+	
+	/**
+	 * Handles a GET request to reserve a specific Cita identified by the given "id" parameter.
+	 * If the Cita does not exist or has already been reserved, a message is added to the redirect
+	 * attributes and the user is redirected to the main page. Otherwise, the Cita is marked as reserved
+	 * and a success message is added to the redirect attributes. Finally, the user is redirected to the main page.
+	 * @author: Benjamín Ruiz
+	 * @param cita the Cita object representing the new Cita to be reserved
+	 * @param attr a RedirectAttributes object used to send flash attributes to the redirected page
+	 * @param model the Model object used to add attributes to the view
+	 * @return a String representing the URL to be redirected
+	 */	
+	@GetMapping("/cancela/{id}")
+	public String cancelarCita(@PathVariable(name="id") int codigo, RedirectAttributes attr) {
+	    Cita cita = citaserv.findById(codigo);
+	    if (cita == null) {
+	        attr.addFlashAttribute("mensajeError", "La cita no existe");
+	        return "redirect:/";
+	    } else if (cita.getDisponible() == 1) {
+	        attr.addFlashAttribute("mensajeError", "La cita no está reservada");
+	        return "redirect:/";
+	    } else {
+	        cita.setDisponible(1);
+	        citaserv.editarCita(cita);
+	        attr.addFlashAttribute("mensajeExito", "La cita está disponible");
+	        return "redirect:/";
+	    }
+	}
+	
+	/**
+	 * Handles a GET requests to delete a Cita with the given ID.
+	 * @author: Benjamín Ruiz
+	 * @param codigo the ID of the Cita to be deleted.
 	 * @param model Object that holds the attributes to be used in the view.
 	 * @param attr Object that contains attributes for the redirect page.
 	 * @return A String representing the redirect page URL.
 	 */
 	@GetMapping("/eliminar/{id}")
-	public String eliminarReserva (Model model, @PathVariable(name="id") int  codigo, RedirectAttributes attr) {
-		if (reserserv.eliminarReserva(codigo) == 1)
-			attr.addFlashAttribute("mensajeExito", "Reserva eliminada");
+	public String eliminar(Model model, @PathVariable(name="id") int  codigo, RedirectAttributes attr) {
+		if (citaserv.eliminarCita(codigo) == 1)
+			attr.addFlashAttribute("mensajeExito", "Cita eliminada");
 		else
-			attr.addFlashAttribute("mensajeError", "Reserva no eliminado");
-		return "forward:/";	 
+			attr.addFlashAttribute("mensajeError", "Cita no eliminada");
+		return "redirect:/";	 
 	}
 	
 	/**
